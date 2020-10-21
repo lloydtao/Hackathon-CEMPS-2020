8edf348952a5f747e0402da780595ae4fdc1edf1
LloydTao <LL23@live.co.uk>
2020-10-21 15:01:46 +0100

    Complete level 4

--- Warm-Ups/level4.py
+++ Warm-Ups/level4.py
@@ -1,13 +1,17 @@
 """ Hackathon - Level 4 """
 
 def double_swap(string, a, b):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+
+    output = []
+    for i in range(0,len(string)):
+        if string[i] == a:
+            output.append(b)
+            continue
+        if string[i] == b:
+            output.append(a)
+            continue
+        output.append(string[i])
+    return "".join(output)
 
 if __name__ == '__main__':
-    # Add any code to test your solution here
-    # As per the example, this should return ienv, ivdv, ivcv
     print(double_swap('veni, vidi, vici', 'v', 'i'))
-    
