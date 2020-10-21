cb9ef3c33f33ab3e52ad28597b76e0d97537e91f
LloydTao <LL23@live.co.uk>
2020-10-21 15:04:07 +0100

    Complete level 5

--- Warm-Ups/level5.py
+++ Warm-Ups/level5.py
@@ -1,13 +1,20 @@
 """ Hackathon - Level 5 """
 
-def convert(numeral):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
 
-if __name__ == '__main__':
+def convert(numeral):
+    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    for i, c in enumerate(numeral):
+        if (i + 1) == len(numeral) or roman_numerals[c] >= roman_numerals[
+            numeral[i + 1]
+        ]:
+            result += roman_numerals[c]
+        else:
+            result -= roman_numerals[c]
+    return result
+
+
+if __name__ == "__main__":
     # Add any code to test your solution here
     # As per the example, this should return 1145
-    print(convert('MCXLV'))
-    
+    print(convert("MCXLV"))
