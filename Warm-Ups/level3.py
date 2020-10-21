a8f738872b5bfc4120e42eeca108062cd1d8e2a6
LloydTao <LL23@live.co.uk>
2020-10-21 14:56:24 +0100

    Complete level 3

--- Warm-Ups/level3.py
+++ Warm-Ups/level3.py
@@ -1,13 +1,18 @@
 """ Hackathon - Level 3 """
 
 def oddish_evenish(x):
-    # Add your solution here. You can use additional functions if need be.
-    # Don't forget to add a DocString for all your functions and comment your code.
-    # Your functions should return values rather than printing the result although you can use printing for testing purposes.
-    return "Your Return Value"
+    num = str(x)
+    total = 0
+    for char in num:
+        total += int(char)
+    print(total)
+    
+    if total % 2 == 0:
+        return "Evenish"
+    return "Oddish"
+
 
 if __name__ == '__main__':
     # Add any code to test your solution here
     # As per the example, this should return Oddish
     print(oddish_evenish(1190))
-    
